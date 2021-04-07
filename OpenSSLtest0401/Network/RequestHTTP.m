e64da32c9706ed7772ab367d11a7c656b0f733cb
timothy <jungun3329@gmail.com>
2021-04-07 17:39:50 +0900

    중간지점 지정용 commit_2

--- OpenSSLtest0401/Network/RequestHTTP.m
+++ OpenSSLtest0401/Network/RequestHTTP.m
@@ -42,8 +42,8 @@
     [request setValue:xToken forHTTPHeaderField:@"X-AUTH-TOKEN"];
     
     NSMutableDictionary *jsonBody = [[NSMutableDictionary alloc] init];
-    [jsonBody setObject:@"value" forKey:@"key"];
-    
+    [jsonBody setObject:@"value1" forKey:@"key1"];
+    [jsonBody setObject:@"value2" forKey:@"key2"];
     
 }
 

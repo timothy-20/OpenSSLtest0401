53b28921562e471a6da44057e176b253211fdd37
timothy-20 <jungun3329@gmail.com>
2021-04-08 16:47:56 +0900

    request 로직 구현 완료, response 작업

---
+++ OpenSSLtest0401/JusTry/GeneratePem.m
@@ -0,0 +1,13 @@
+//
+//  GeneratePem.m
+//  OpenSSLtest0401
+//
+//  Created by 임정운 on 2021/04/08.
+//  Copyright © 2021 임정운. All rights reserved.
+//
+
+#import "GeneratePem.h"
+
+@implementation GeneratePem
+
+@end
